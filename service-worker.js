c144517043eeb09ce37ee73e542d4c81ba99c3ee
codegeass25 <angelroy.longakit@deped.gov.ph>
2026-08-23 09:31:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
      service-worker update.
    ===================================================================== */
 
-const CACHE_NAME = "qlogpro-offline-v21-superfast-central-sync-profilehandoff";
+const CACHE_NAME = "qlogpro-offline-v22-face-central-recognition";
 
 const PRECACHE_URLS = [
   "./",
